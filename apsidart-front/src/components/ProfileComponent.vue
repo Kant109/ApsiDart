ab64ac1cccb6a1c768a0914f9ee98d3e7b95c04c
Quentin ROULET <quentin.roulet1@gmail.com>
2024-08-23 12:00:51 +0200

    Add style user page

--- apsidart-front/src/components/ProfileComponent.vue
+++ apsidart-front/src/components/ProfileComponent.vue
@@ -1,11 +1,41 @@
 <script setup lang="ts">
+import { ref } from "vue";
+import gsap from "gsap";
 import User from "../assets/mock/user.json";
+import { Flip } from "gsap/Flip";
+
+gsap.registerPlugin(Flip)
+
+const seeMore = ref(false);
+
+const seeProfile = () => {
+    seeMore.value = !seeMore.value;
+}
+
+const beforeEnter = (el: any) => {
+    el.style.transform = 'translateY(-60px)'
+    el.style.opacity = 0
+}
+
+const enter = (el: any, done: any) => {
+    gsap.to(el, {
+        duration: 1,
+        y: 0,
+        opacity: 1,
+        ease: 'bounce.out',
+        onComplete: done
+    })
+}
+
+const saveUser = () => {
+    console.log('in save');
+}
 </script>
 
 <template>
-    <div class="profile-container">
+    <div class="profile-container" v-if="!seeMore">
         <div class="see-more">
-            <img src="../assets/images/see-more.svg" alt="Voir plus">
+            <img src="../assets/images/see-more.svg" alt="Voir plus" @click.prevent="seeProfile">
         </div>
         <div class="profile-infos-container">
             <div class="profile-picture">
@@ -15,10 +45,41 @@ import User from "../assets/mock/user.json";
                 <div class="name">{{ User.name }}</div>
                 <div class="firstname">{{ User.firstname }}</div>
                 <div class="ratio">Ratio (V/D) : &nbsp;<div class="value">{{ User.win }}/{{ User.loose }}</div></div>
-                <div class="rank">Classement : &nbsp;<div class="value">{{ User.rank }}</div>ème</div>
+                <div class="rank">Classement : &nbsp;<span class="value">{{ User.rank }}<sup>ème</sup></span></div>
             </div>
         </div>
     </div>
+    <transition
+        v-else
+        appear
+        @before-enter="beforeEnter"
+        @enter="enter"
+        :css="false"
+    >
+        <div class="user-profile-container">
+            <div class="user-picture">
+                <img src="../assets/mock/user-picture.svg" alt="Photo de profil">
+            </div>
+            <div class="input name">
+                <label for="name">Nom</label>
+                <input type="text" id="name" name="name" v-model="User.name"/>
+            </div>
+            <div class="input firstname">
+                <label for="firstname">Prénom</label>
+                <input type="text" id="firstname" name="firstname" v-model="User.firstname"/>
+            </div>
+            <div class="input birthdate">
+                <label for="birthdate">Date de naissance</label>
+                <input type="text" id="birthdate" name="birthdate" v-model="User.birthdate"/>
+            </div>
+            <div class="input mail">
+                <label for="mail">Email</label>
+                <input type="text" id="mail" name="mail" v-model="User.mail"/>
+            </div>
+            <div class="save" @click.prevent="saveUser">Btn Save</div>
+            <div class="cancel" @click.prevent="seeProfile">Annuler</div>
+        </div>
+    </transition>
 </template>
 
 <style lang="scss">
@@ -48,17 +109,15 @@ import User from "../assets/mock/user.json";
 
         .profile-picture {
             display: flex;
-            background-color: red;
-            border-radius: 50%;
-            width: 6rem;
-            height: 6rem;
             justify-content: center;
             align-items: center;
-
+            
             img {
-                width: 5rem;
-                height: 5rem;
+                width: 6rem;
+                height: 6rem;
                 aspect-ratio: 1/1;
+                background-color: grey;
+                border-radius: 50%;
             }
 
         }
@@ -84,7 +143,70 @@ import User from "../assets/mock/user.json";
                 .value {
                     font-family: "Bungee Tint", sans-serif;
                 }
+
+                sup {
+                    font-family: "Kanit", sans-serif;
+                }
             }
+        }
+    }
+}
+
+.user-profile-container {
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+    margin: 1rem;
+    padding: 2rem;
+    border-radius: 10px;
+    box-shadow: 0px 0px 50px 2px rgba(0,0,0,0.5);
+
+    .user-picture {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+
+        img {
+            width: 7rem;
+            height: 7rem;
+            aspect-ratio: 1/1;
+            border-radius: 50%;
+            background-color: grey;
+        }
+
+    }
+
+    .input {
+        display: flex;
+        flex-direction: column;
+        position: relative;
+        border: 1px solid lightgrey;
+        border-radius: 5px;
+        height: 3rem;
+        max-width: 100%;
+
+        label {
+            position: absolute;
+            top: -.7rem;
+            background-color: white;
+            margin-left: .5rem;
+            text-align: center;
+            font-family: "Kanit", sans-serif;
+            font-size: 1rem;
+        }
+
+        input {
+            border: none;
+            font-family: "Kanit", sans-serif;
+            font-size: 1.5rem;
+            height: 100%;
+            padding: .5rem;
+            margin: 0 .5rem;
+            outline: none;
+        }
+
+        &:focus {
+            border: 1px solid black;
         }
     }
 }
