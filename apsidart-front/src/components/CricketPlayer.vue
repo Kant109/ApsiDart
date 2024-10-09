b9c4132e9f3fdcf27914bc5ba0e5ea42632abf56
Quentin ROULET <quentin.roulet1@gmail.com>
2024-10-09 09:58:21 +0200

    Alignement cricket points

--- apsidart-front/src/components/CricketPlayer.vue
+++ apsidart-front/src/components/CricketPlayer.vue
@@ -5,11 +5,60 @@ defineProps<{
 </script>
 
 <template>
-    <div class="player-name">{{ player }}</div>
+    <div class="player-content">
+        <div class="player-name">{{ player }}</div>
+        <div class="doors">
+            <div class="door"></div>
+            <div class="door"></div>
+            <div class="door"></div>
+            <div class="door"></div>
+            <div class="door"></div>
+            <div class="door"></div>
+            <div class="door"></div>
+        </div>
+        <div class="points-taken">540</div>
+    </div>
 </template>
 
 <style lang="scss">
-.player-name {
-    color: #F0F2EF;
+.player-content {
+    display: grid;
+    grid-template-columns: repeat(5, 1fr);
+    grid-template-rows: 1fr;
+    grid-column-gap: 0px;
+    grid-row-gap: 0px;
+    height: 60px;
+
+    .player-name, .points-taken {
+        display: flex;
+        align-items: center;
+        font-family: "Tilt Warp", sans-serif;
+        font-size: 1rem;
+        color: rgba(black, .75);
+
+        &:is(.player-name) {
+            min-width: 55px;
+            grid-area: 1 / 1 / 2 / 2;
+        }
+
+        &:is(.points-taken) {
+            justify-content: flex-end;
+            grid-area: 1 / 5 / 2 / 6;
+        }
+    }
+
+    .doors {
+        display: flex;
+        align-items: center;
+        gap: .5rem;
+        grid-area: 1 / 2 / 2 / 5;
+
+        .door {
+            border: 2px solid black;
+            border-radius: 50%;
+            height: 1.5rem;
+            width: 1.5rem;
+        }
+    }
 }
 </style>
--- apsidart-front/src/views/CricketGame.vue
+++ apsidart-front/src/views/CricketGame.vue
@@ -3,13 +3,15 @@ import { ref } from 'vue';
 import CricketBoard from '@/components/CricketBoard.vue';
 import CricketPlayer from '@/components/CricketPlayer.vue';
 
-const players = ref(['Kant1','Mati', 'ROM1']);
+const players = ref(['KKKKK','Mati', 'ROM1']);
 </script>
 
 <template>
     <div class="title">CRICKET</div>
-    <div class="players">
-        <CricketPlayer v-for="player in players" :player="player"/>
+    <div class="players-container">
+        <div class="players-content">
+            <CricketPlayer v-for="player in players" :player="player"/>
+        </div>
     </div>
     <CricketBoard />
 </template>
@@ -22,5 +24,20 @@ const players = ref(['Kant1','Mati', 'ROM1']);
     font-size: 3.25rem;
     padding: 2rem;
     color: #F0F2EF;
+}
+
+.players-container {
+    display: flex;
+    justify-content: center;
+    width: 100%;
+    background-color: #F0F2EF;
+
+    .players-content {
+        display: flex;
+        flex-direction: column;
+        padding: 0 1rem;
+        max-width: 390px;
+        width: 100%;
+    }
 }
 </style>
