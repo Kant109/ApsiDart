59ee765ebf04a7b6ff26838828a9f9ed2655c2f0
Quentin ROULET <quentin.roulet1@gmail.com>
2024-10-14 17:47:03 +0200

    Try PWA

--- apsidart-front/vite.config.ts
+++ apsidart-front/vite.config.ts
@@ -3,12 +3,29 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'ApsiDart',
+        short_name: 'ApsiDart',
+        description: 'Application de fléchettes d\'Apside',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'icons/target.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+        ],
+      },
+    }),
   ],
   resolve: {
     alias: {
