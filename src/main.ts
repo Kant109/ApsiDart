1f4285252110133e8b98b9043b4b87f126b608ef
Quentin ROULET <quentin.roulet1@gmail.com>
2024-11-05 11:10:33 +0100

    Update PWA settings

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,8 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+import { registerSW } from 'virtual:pwa-register'
+registerSW({ immediate: true })
 
 const app = createApp(App)
 
@@ -12,3 +14,4 @@ app.use(createPinia())
 app.use(router)
 
 app.mount('#app')
+
