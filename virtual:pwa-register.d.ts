8678b7109d25ed48f70b6d7f40a913476d2324aa
Quentin ROULET <quentin.roulet1@gmail.com>
2024-11-05 11:31:47 +0100

    Update PWA settings

---
+++ virtual:pwa-register.d.ts
@@ -0,0 +1,7 @@
+declare module 'virtual:pwa-register' {
+    import type { RegisterSWOptions } from 'vite-plugin-pwa/types'
+  
+    export type { RegisterSWOptions }
+  
+    export function registerSW(options?: RegisterSWOptions): (reloadPage?: boolean) => Promise<void>
+}
