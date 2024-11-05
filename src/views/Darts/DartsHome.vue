95579ed9670e7473dc659bc6c608616813f05f11
Quentin ROULET <quentin.roulet1@gmail.com>
2024-11-05 16:33:06 +0100

    Add btn + change class animation name

--- src/views/Darts/DartsHome.vue
+++ src/views/Darts/DartsHome.vue
@@ -1,10 +1,13 @@
 <script setup lang="ts">
 import { onMounted, ref } from 'vue';
 
-const isAnimation = ref(false);
+const isSlideTopAnimation = ref(false);
 
 onMounted(() => {
-    isAnimation.value = true;
+    isSlideTopAnimation.value = true;
+    setTimeout(() => {
+        isSlideTopAnimation.value = false;
+    }, 500);
 })
 </script>
 
@@ -14,11 +17,15 @@ onMounted(() => {
             <div class="title">DARTS</div>
         </div>
 
-        <div class="darts-img" :class="{'animation': isAnimation}">
+        <div class="darts-img" :class="{'slide-top-animation': isSlideTopAnimation}">
             <img src="/icons/darts.png" alt="Image Game">
         </div>
 
         <div class="darts-game-chose-mode">
+            <div class="choices-container">
+                <div class="choice">Cricket</div>
+                <div class="choice">X01</div>
+            </div>
         </div>
     </div>
 </template>
@@ -38,32 +45,32 @@ onMounted(() => {
         width: 100%;
         padding: 1rem .5rem;
         background-color: var(--bg-color-primary);
-        animation-duration: .5s;
-        animation-name: appear;
+        animation: appear .5s;
 
         .title {
             display: flex;
             justify-content: center;
             font-family: "Monoton", sans-serif;
-            font-size: 2rem;
+            font-size: 3rem;
             color: var(--text-color);
         }
     }
 
     .darts-img {
-        position: absolute;
-        top: 50%;
-        left: 50%;
-        transform: translate(-50%, -50%);
+        display: flex;
+        align-items: center;
+        justify-content: center;
         background-color: var(--bg-color-secondary);
         border-radius: .5rem;
         width: 9rem;
         height: 9rem;
+        margin-top: 1rem;
         
-        &.animation {
-            display: flex;
-            align-items: center;
-            justify-content: center;
+        &.slide-top-animation {
+            position: absolute;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%, -50%);
             animation: slide-top 0.5s cubic-bezier(0.250, 0.460, 0.450, 0.940) both;
         }
 
@@ -75,6 +82,41 @@ onMounted(() => {
 
     .darts-game-chose-mode {
         display: flex;
+        width: 100%;
+        animation: appear 1.25s;
+        margin: 3rem 0;
+
+        .choices-container {
+            display: flex;
+            align-items: center;
+            justify-content: space-around;
+            width: 100%;
+
+            .choice {
+                display: flex;
+                align-items: center;
+                justify-content: center;
+                background-color: var(--bg-color-secondary);
+                height: 60px;
+                width: 130px;
+                border-radius: 1rem;
+
+                font-family: "Tilt Warp", sans-serif;
+                font-size: 1.5rem;
+                color: var(--text-color);
+                padding-bottom: 5px;
+
+                --tw-shadow: inset 0 -5px 0 0 rgba(0, 0, 0, .25);
+                box-shadow: var(--tw-ring-offset-shadow, 0 0 #0000), var(--tw-ring-shadow, 0 0 #0000), var(--tw-shadow);
+                border: 1px solid rgba(0, 0, 0, .25);
+
+                &:active {
+                    color: rgba(black, .25);
+                    transform: translateY(5px);
+                    box-shadow: none;
+                }
+            }
+        }
     }
 }
 
@@ -89,6 +131,9 @@ onMounted(() => {
 
 @keyframes appear {
     0% {
+        opacity: 0;
+    }
+    80% {
         opacity: 0;
     }
     100% {
