728787ad041ac823b8ea02a428c5e5c8870bbad2
Quentin ROULET <quentin.roulet1@gmail.com>
2024-12-24 14:09:18 +0100

    Test en prod (logique)

--- src/App.vue
+++ src/App.vue
@@ -18,6 +18,7 @@ const isAppLoaded = computed(() => managementAppStore.isAppLoaded);
 const isDarkMode = computed(() => managementAppStore.isDarkMode);
 
 onBeforeMount(() => {
+    localStorage.removeItem('orderedDartsPlayer')
     managementAppStore.isDarkMode = localStorage.getItem('darkmode-apsidart') === 'active';
 })
 
